d7b05c792a8176516d645f546a605e8cfb7fecf5
Baray44 <baray44@mail.ru>
2022-10-29 23:47:22 +0300

    add: tasks, solutions

---
+++ SQL/1. The relational model/solutions.sql
@@ -0,0 +1,28 @@
+/* Task_1 MySQL 
+CREATE TABLE book(
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title   VARCHAR(50),
+    author  VARCHAR(30),
+    price   DECIMAL(8, 2),
+    amount  INT
+);
+*/
+
+/* Task_1 Postgres */
+CREATE TABLE book(
+    book_id SERIAL PRIMARY KEY,
+    title   VARCHAR(50),
+    author  VARCHAR(30),
+    price   DECIMAL(8, 2),
+    amount  INT
+);
+
+/* Task_2 */
+INSERT INTO book(title, author, price, amount) 
+VALUES ("Мастер и Маргарита", "Булгаков М.А.", 670.99, 3);
+
+/* Task_3 */
+INSERT INTO book(title, author, price, amount)
+VALUES ("Белая гвардия", "Булгаков М.А.", 540.50, 5),
+       ("Идиот", "Достоевский Ф.М.", 460.00, 10),
+       ("Братья Карамазовы", "Достоевский Ф.М.", 799.01, 2)
