5dee23ba0113b19497f30bbf0797ba9f7a3e08fa
Baray44 <baray44@mail.ru>
2022-11-07 00:09:41 +0300

    fix: 1.6 solutions

--- 1. The relational model/1.6 Trip table/solutions.sql
+++ 1. The relational model/1.6 Trip table/solutions.sql
@@ -88,16 +88,35 @@ ORDER BY city, name;
 /* Вывести название месяца и количество командировок для каждого месяца. 
    Считаем, что командировка относится к некоторому месяцу, если она началась в этом месяце. Информацию вывести сначала в отсортированном 
    по убыванию количества, а потом в алфавитном порядке по названию месяца виде. Название столбцов – Месяц и Количество. */
-SELECT MONTHNAME(date_first) AS 'Месяц', COUNT(*) AS 'Количество'
+SELECT MONTHNAME(date_first) AS Месяц, COUNT(*) AS Количество
 FROM trip
 GROUP BY MONTHNAME(date_first)
-ORDER BY 'Количество' DESC, 'Месяц';
+ORDER BY Количество DESC, Месяц;
 
 /* Вывести сумму суточных (произведение количества дней командировки и размера суточных) для командировок, первый день которых пришелся на февраль или март 2020 года. 
    Значение суточных для каждой командировки занесено в столбец per_diem. Вывести фамилию и инициалы сотрудника, город, первый день командировки и сумму суточных. 
    Последний столбец назвать Сумма. Информацию отсортировать сначала в алфавитном порядке по фамилиям сотрудников, а затем по убыванию суммы суточных. */
-SELECT `name`, city, date_first, ((DATEDIFF(date_last, date_first) + 1) * per_diem) AS 'Сумма'
+-- 1 variant
+SELECT `name`, city, date_first, ((DATEDIFF(date_last, date_first) + 1) * per_diem) AS Сумма
 FROM trip
 WHERE date_first LIKE '2020-02%' OR date_first LIKE '2020-03%'
-ORDER BY `name`, 'Сумма' DESC;
-   
+ORDER BY `name`, Сумма DESC;
+-- 2 variant
+SELECT `name`, city, date_first, per_diem * (DATEDIFF(date_last, date_first) + 1) AS Сумма
+FROM trip
+WHERE MONTHNAME(date_first) = 'February'
+OR MONTHNAME(date_first) = 'March'
+ORDER BY `name`, Сумма DESC
+
+/* Вывести фамилию с инициалами и общую сумму суточных, полученных за все командировки 
+   для тех сотрудников, которые были в командировках больше чем 3 раза, 
+   в отсортированном по убыванию сумм суточных виде. Последний столбец назвать Сумма. */
+SELECT name, SUM((DATEDIFF(date_last, date_first) + 1) * per_diem) AS Сумма
+FROM trip
+GROUP BY name
+HAVING name IN (
+    SELECT name
+    FROM trip
+    GROUP BY name
+    HAVING COUNT(name) > 3)
+ORDER BY Сумма DESC;
