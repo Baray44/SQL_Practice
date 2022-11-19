930261b9960201346de81b647a668688e9b3a225
Baray44 <baray44@mail.ru>
2022-11-19 23:10:42 +0300

    fix: 2.4 solutions

--- 2. SQL queries to related tables/2.4 Online store database (select queries)/solutions.sql
+++ 2. SQL queries to related tables/2.4 Online store database (select queries)/solutions.sql
@@ -29,3 +29,18 @@ GROUP BY name_city
 ORDER BY 'Количество' DESC, name_city;
 
 /* Вывести номера всех оплаченных заказов и даты, когда они были оплачены. */
+SELECT buy_id, date_step_end
+FROM step
+JOIN buy_step USING (step_id)
+WHERE name_step LIKE 'Оплата' AND date_step_end IS NOT NULL;
+
+/* Вывести информацию о каждом заказе: его номер, кто его сформировал (фамилия пользователя) 
+   и его стоимость (сумма произведений количества заказанных книг и их цены), в отсортированном 
+   по номеру заказа виде. Последний столбец назвать Стоимость. */
+SELECT buy_id, name_client, SUM(price * buy_book.amount) AS 'Стоимость'
+FROM book
+JOIN buy_book USING (book_id)
+JOIN buy USING (buy_id)
+JOIN `client` USING (client_id)
+GROUP BY buy_id, name_client
+ORDER BY buy_id;
