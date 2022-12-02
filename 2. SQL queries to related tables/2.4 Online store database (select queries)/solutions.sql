0bd0554cc20499267188e334cf559ab938ca0c71
Baray44 <baray44@mail.ru>
2022-12-02 15:44:02 +0300

    fix: 2.4 solutions

--- 2. SQL queries to related tables/2.4 Online store database (select queries)/solutions.sql
+++ 2. SQL queries to related tables/2.4 Online store database (select queries)/solutions.sql
@@ -89,3 +89,70 @@ WHERE name_author = 'Достоевский Ф.М.'
 ORDER BY name_client;
 
 /* Вывести жанр (или жанры), в котором было заказано больше всего экземпляров книг, указать это количество. Последний столбец назвать Количество. */
+SELECT genre.name_genre, SUM(buy_book.amount) as Количество
+FROM genre
+    JOIN book USING(genre_id)
+    JOIN buy_book USING(book_id)
+GROUP BY genre.name_genre
+HAVING SUM(buy_book.amount) =
+    (/* вычисляем максимальный жанр из общего количества каждого заказа */
+        SELECT MAX(sum_amount) AS max_sum_amount
+        FROM 
+            (/* считаем количество жанров каждого заказа */
+            SELECT genre.genre_id, SUM(buy_book.amount) AS sum_amount 
+            FROM genre
+            JOIN book USING(genre_id)
+            JOIN buy_book USING(book_id)
+            GROUP BY genre.genre_id
+            ) query_in
+    );
+    
+/* Сравнить ежемесячную выручку от продажи книг за текущий и предыдущий годы.
+   Для этого вывести год, месяц, сумму выручки в отсортированном сначала по возрастанию месяцев, затем по возрастанию лет виде.
+   Название столбцов: Год, Месяц, Сумма. */
+SELECT YEAR(date_payment) AS Год, MONTHNAME(date_payment) AS Месяц, SUM(price * amount) AS Сумма
+FROM buy_archive
+GROUP BY Год, Месяц
+
+UNION ALL
+
+SELECT YEAR(date_step_end) AS Год, MONTHNAME(date_step_end) AS Месяц, SUM(book.price * buy_book.amount) AS Сумма
+FROM buy_step
+JOIN buy_book USING (buy_id)
+JOIN book USING (book_id)
+WHERE date_step_end IS NOT NULL AND step_id = 1
+GROUP BY Год, Месяц
+ORDER BY Месяц, Год;
+
+/* Для каждой отдельной книги необходимо вывести информацию о количестве проданных экземпляров и их стоимости за текущий и предыдущий год.
+   Вычисляемые столбцы назвать Количество и Сумма. Информацию отсортировать по убыванию стоимости. */
+SELECT title, SUM(Количество) AS Количество, SUM(Сумма) AS Сумма
+FROM
+    (SELECT title, SUM(buy_book.amount) AS Количество, SUM(book.price * buy_book.amount) AS Сумма
+    FROM buy_book 
+    JOIN buy USING (buy_id)
+    JOIN buy_step USING (buy_id)
+    JOIN step USING (step_id)
+    JOIN book USING (book_id)
+    WHERE name_step = 'Оплата' AND not isNull(date_step_end)
+    GROUP BY title
+     
+    UNION
+     
+    SELECT title, SUM(buy_archive.amount) AS amount, SUM(buy_archive.price * buy_archive.amount) AS total
+    FROM buy_archive
+    JOIN book ON buy_archive.book_id = book.book_id
+    GROUP BY title ) AS query
+GROUP BY title
+ORDER BY Сумма DESC;
+
+/* Вывести названия книг, которые ни разу не были заказаны, отсортировав в алфавитном порядке. */
+SELECT title_b.title
+FROM 
+	(SELECT book.title , title_buy.title AS t2
+	 FROM book
+     LEFT JOIN (SELECT title, book_id
+			    FROM buy_book
+			    JOIN book USING(book_id)
+			    GROUP BY title, book_id) AS title_buy USING (book_id)) AS title_b
+WHERE title_b.t2 IS NULL;
